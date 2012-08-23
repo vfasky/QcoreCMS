e605a45222570bde8f51c04b62137099973556e1
vfasky <vfaky@gmail.com>
2012-08-23 23:40:39 +0800

    加入安装脚本

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup
+
+setup(
+    name = "Qcore-cms",
+    version = "1.0 DEV",
+    author = "vfasky",
+    author_email = "vfasky@gmail.com",
+    description = ("基于 tornado 的 cms"),
+    install_requires = [
+        'tornado',
+        'MySQL-python'
+        ],
+    platforms='any',
+    license = "",
+    url = "http://vfasky.com",
+    packages=[],
+    scripts = [],
+)
